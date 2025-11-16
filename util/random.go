23d99ffd8042e0d5dbda27c43ba2642189519892
laiys <xiezhaoyang@gmail.com>
2025-11-16 10:53:16 +0800

    加强随机

--- util/random.go
+++ util/random.go
@@ -8,10 +8,14 @@ import (
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GenerateRandomString(n int) string {
-	rand.Seed(time.Now().UnixNano())
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = charset[rand.Intn(len(charset))]
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var result []byte
+
+	for i := 0; i < n; i++ {
+		index := seededRand.Intn(len(charset))
+		result = append(result, charset[index])
 	}
-	return string(b)
+
+	return string(result)
 }
